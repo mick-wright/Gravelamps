41818e701eb06675810df084e72c285d4f71e0f6
Mick Wright <mick.wright@protonmail.com>
2021-06-18 14:16:25 +0100

    Inference and Utils refactor and linting

--- gwlensing/inference/inference.py
+++ gwlensing/inference/inference.py
@@ -1,100 +1,149 @@
+'''
+Gravelamps
+
+Local machine lens data generation and analysis variant
+for Simulated Data
+
+Written by Mick Wright 2021
+'''
+
+import os
 import sys
-import os 
-import numpy as np 
-import bilby 
-import gwlensing.lensing 
+import importlib
 
 from configparser import ConfigParser
 
+import bilby
+
+import gwlensing.lensing
+
 def main():
-	#Instantiate the Configuration Parser
-	config = ConfigParser()
+    '''
+    Main function - takes the user generated ini and generates the lens data
+    and performs an analysis run
+    '''
 
-	#If user hasn't given a useable ini file, raise exception
-	if not os.path.isfile(sys.argv[1]):
-		raise IOError("Input ini file not given")
-	
-	#Check that the Configuration Parser can read the ini file 
-	try:
-		config.read(sys.argv[1])
-	except IOError:
-		print("Ini file unreadable!") 
+    #Instantiate the Configuration Parser
+    config = ConfigParser()
 
-	#Bilby Logging Set Up 
-	label = config.get("bilby_setup","label")
-	outdir = config.get("bilby_setup","outdir")
+    #If the user hasn't given a useable ini file, raise excpetion
+    if not os.path.isfile(sys.argv[1]):
+        raise IOError("Input ini file not given!")
 
-	bilby.core.utils.setup_logger(label=label, outdir=outdir)
-	
-	data_subdir = config.get("data_settings","data_subdir")
-	if not os.path.isdir(outdir+"/"+data_subdir):
-		os.mkdir(outdir+"/"+data_subdir) 
+    #Check that the Configuration Parser can read the ini file
+    try:
+        config.read(sys.argv[1])
+    except IOError:
+        print("Input ini file is unreadable!")
 
-	#Read in User Parameters for Bilby Analysis
-	duration = config.getfloat("bilby_setup","duration")
-	sampling_frequency = config.getfloat("bilby_setup","sampling_frequency") 
+    #Bilby Logging Set Up
+    label = config.get("bilby_setup", "label")
+    outdir = config.get("bilby_setup", "outdir")
 
-	#Read in Waveform Parameters and convert to floats
-	injection_parameters = config._sections["base_waveform_injection_parameters"].copy()
-	waveform_arguments = config._sections["waveform_arguments"].copy()
+    bilby.core.utils.setup_logger(label=label, outdir=outdir)
 
-	waveform_arguments["reference_frequency"] = float(waveform_arguments["reference_frequency"]) 
-	waveform_arguments["minimum_frequency"] = float(waveform_arguments["minimum_frequency"]) 
-	injection_parameters.update((key, float(value)) for key, value in injection_parameters.items()) 
+    #If the data subdirectory doesn't exist, create it
+    data_subdir = config.get("data_settings", "data_subdir")
+    data_fullpath = outdir + "/" + data_subdir
 
-	injection_parameters["chirp_mass"] = bilby.gw.conversion.component_masses_to_chirp_mass(injection_parameters["mass_1"], injection_parameters["mass_2"]) 
+    if not os.path.isdir(data_fullpath):
+        os.mkdir(data_fullpath)
 
-	#Get the Dimensionless Frequency and Impact Parameter Files 
-	w_array_file, y_array_file = gwlensing.lensing.utils.wyhandler(config, injection_parameters)
+    #Read in User Parameters for the Bilby Analysis
+    duration = config.getfloat("bilby_setup", "duration")
+    sampling_frequency = config.getfloat("bilby_setup", "sampling_frequency")
 
-	#Get the Lens Model
-	lens_model = config.get("lens_settings", "lens_model") 
-	#Get Additional Lensing Parameters if needed
-	additional_lens_parameters = gwlensing.lensing.utils.get_additional_parameters(config) 
+    #Read in the Waveform parameters and Convert to Floats
+    injection_parameters = config._sections["base_waveform_injection_parameters"].copy()
+    waveform_arguments = config._sections["waveform_arguments"].copy()
 
-	#Get the Amplification Factor Files
-	amp_fac_real_file, amp_fac_imag_file = gwlensing.lensing.utils.ampfachandler(config,injection_parameters, w_array_file, y_array_file, lens_model, additional_lens_parameters)
+    for key in ["reference_frequency", "minimum_frequency"]:
+        waveform_arguments[key] = float(waveform_arguments[key])
 
-	#Generate the Lens Interpolator Function and add it to the waveform arguments 
-	lens_interpolator = gwlensing.lensing.utils.generate_interpolator(w_array_file, y_array_file, amp_fac_real_file, amp_fac_imag_file)
-	waveform_arguments["interpolator"] = lens_interpolator 
+    injection_parameters.update((key, float(value)) for key, value in injection_parameters.items())
 
-	#Generate the Lensed Waveform
-	wfgen = bilby.gw.WaveformGenerator(duration=duration, sampling_frequency=sampling_frequency, frequency_domain_source_model=gwlensing.lensing.BBH_lensed_waveform, parameter_conversion=bilby.gw.conversion.convert_to_lal_binary_black_hole_parameters, waveform_arguments=waveform_arguments)
+    #Add the Chirp Mass and Mass Ratio values to the injection parameters
+    injection_parameters["chirp_mass"] = bilby.gw.conversion.component_masses_to_chirp_mass(
+            injection_parameters["mass_1"], injection_parameters["mass_2"])
+    injection_parameters["mass_ratio"] = bilby.gw.conversion.component_masses_to_mass_ratio(
+            injection_parameters["mass_1"], injection_parameters["mass_2"])
 
-	#Create Interferometers and inject lensed signal
-	interferometer_list = config.get("bilby_setup","detectors").replace(" ","").split(",")
-	ifos = bilby.gw.detector.InterferometerList(interferometer_list) 
-	ifos.set_strain_data_from_power_spectral_densities(sampling_frequency=sampling_frequency,duration=duration, start_time=injection_parameters["geocent_time"]-3)
-	ifos.inject_signal(waveform_generator=wfgen, parameters=injection_parameters) 
+    #Get the Dimensionless Frequency and Impact Parameter Files
+    w_array_file, y_array_file = gwlensing.lensing.utils.wy_handler(config)
 
-	#Likelihood 
-	likelihood = bilby.gw.GravitationalWaveTransient(interferometers=ifos, waveform_generator=wfgen)
-	
-	#Load in Prior File and Fix Specified Parameters 
-	priors = bilby.core.prior.PriorDict(config.get("prior_settings","prior_file")) 
-	prior_fix_list = config.get("prior_settings","parameters_to_fix").replace(" ","").split(",") 
-	for parameter in prior_fix_list:
-		priors[parameter] = injection_parameters[parameter] 
+    #Generate Lensed Interpolator
+    amp_fac_real_file, amp_fac_imag_file = gwlensing.lensing.utils.amp_fac_handler(
+            config, w_array_file, y_array_file, mode="local")
 
-	#Run Sampler, if user has specified generate a corner plot
-	result = bilby.run_sampler(likelihood=likelihood, priors=priors, sampler=config.get("sampler_settings","sampler"), nlive=config.getint("sampler_settings","nlive"), npool=config.getint("sampler_settings","npool"),nact=config.getint("sampler_settings","nact"),nparallel=config.getint("sampler_settings","nparallel"),injection_parameters=injection_parameters, outdir=config.get("bilby_setup","outdir"), label=config.get("bilby_setup","label"))
-	if config.getboolean("bilby_setup","make_corner") == True:
-		result.plot_corner() 
+    #Generate Lensed Waveform
+    lensed_waveform_generator = gwlensing.lensing.Lensed_Waveform_Generator(
+            duration=duration, sampling_frequency=sampling_frequency,
+            frequency_domian_source_model = gwlensing.lensing.BBH_lensed_waveform,
+            parameter_conversion = bilby.gw.conversion.convert_to_lal_binary_black_hole_parameters,
+            waveform_arguments = waveform_arguments)
 
-	#Generate a "Treat Unlensed" if user has specified they want one
-	if config.getboolean("treat_unlensed_settings","create_treat_unlensed") == True: 
-		wfgen_unlensed = bilby.gw.WaveformGenerator(duration=duration, sampling_frequency=sampling_frequency, frequency_domain_source_model=bilby.gw.source.lal_binary_black_hole, parameter_conversion=bilby.gw.conversion.convert_to_lal_binary_black_hole_parameters, waveform_arguments=waveform_arguments)
+    #Generate Injected Interformeters
+    interferometer_list = config.get("bilby_setup", "detectors").replace(" ","").split(",")
+    interferometers = bilby.gw.detector.InterferometerList(interferometer_list)
+    interferometers.set_strain_data_from_power_spectral_densities(
+            sampling_frequency = sampling_frequency, duration=duration,
+            start_time = injection_parameters["geocent_time"] - 3)
+    interferometers.inject_signal(waveform_generator=lensed_waveform_generator,
+            parameters = injection_parameters)
 
-		likelihood_unlensed = bilby.gw.GravitationalWaveTransient(interferometers=ifos, waveform_generator=wfgen_unlensed)
+    #Loading in the Prior File, Fixing Any Parameters Specified
+    prior_file = config.get("prior_settings", "prior_file")
+    prior_fix_list = config.get("prior_settings", "parameters_to_fix").replace(" ","").split(",")
 
-		#For Unlensed Priors, fix all lens parameters 
-		priors_unlensed = priors
-		for parameter in ["lens_mass","impact_parameter","lens_fractional_distance"]:
-			priors_unlensed[parameter] = injection_parameters[parameter] 
+    priors = bilby.core.prior.PriorDict(prior_file)
+    for parameter in prior_fix_list:
+        priors[parameter] = injection_parameters[parameter]
 
-		result_unlensed = bilby.run_sampler(likelihood=likelihood_unlensed, priors=priors_unlensed, sampler=config.get("sampler_settings","sampler"), nlive=config.getint("sampler_settings","nlive"), npool=config.getint("sampler_settings","npool"),nact=config.getint("sampler_settings","nact"),nparallel=config.getint("sampler_settings","nparallel"),injection_parameters=injection_parameters, outdir=config.get("treat_unlensed_settings","unlensed_outdir"), label=config.get("treat_unlensed_settings","unlensed_label"))
+    #Sampler Settings
+    sampler = config.get("sampler_settings", "sampler")
+    plot_corner = config.getboolean("sampler_settings", "plot_corners")
+    sampler_kwargs_dict = config.get("sampler_settings", "sampler_kwargs_dict")
 
-		#Generate a "Treat Unlensed" Corner Plot if user desires
-		if config.getboolean("treat_unlensed_settings","unlensed_make_corner") == True:
-			result_unlensed.plot_corner() 
+    #If unlensed prep run, do this
+    if config.getboolean("unlensed_run_settings", "create_unlensed_prep_run"):
+        #Generate the Unlensed Waveform
+        unlensed_waveform_generator = bilby.gw.WaveformGenerator(
+                duration = duration, sampling_frequency = sampling_frequency,
+                frequency_domain_source_model = bilby.gw.source.lal_binary_black_hole,
+                parameter_conversion = bilby.gw.conversion.convert_to_lal_binary_black_hole_parameters,
+                waveform_arguments = waveform_arguments)
+
+        #Get the Unlensed Likelihoods
+        unlensed_likelihood = bilby.gw.GravitationalWaveTransient(
+                interferometers = interferometers,
+                waveform_generator = unlensed_waveform_generator)
+
+        #Fix Lens Priors for Unlensed Case
+        unlensed_priors = priors
+
+        for parameter in ["lens_mass", "impact_parameter", "lens_fractional_distance"]:
+            unlensed_priors[parameter] = 0
+
+        #Perform the Unlensed Run
+        result_unlensed = bilby.run_sampler(
+                likelihood = unlensed_likelihood, priors = unlensed_priors,
+                outdir = outdir, label = label+"_unlensed", sampler = sampler,
+                plot = plot_corner, injection_parameters = injection_parameters,
+                **sampler_kwargs_dict)
+
+        #TODO: Get either the maximum posterior parameter values, or the full posteriors to use as priors
+
+    #Lensed run
+    #Get the Likelihood
+    lensed_likelihood = bilby.gw.GravitationalWaveTransient(
+            interferometers = interferometers,
+            waveform_generator = lensed_waveform_generator)
+
+    #Run the Sampler
+    result_lensed = bilby.run_sampler(
+            likelihood = lensed_likelihood, priors = priors,
+            outdir = outdir, label = label, sampler = sampler,
+            plot = plot_corner, injection_parameters = injection_parameters,
+            **sampler_kwargs_dict)
+
+
