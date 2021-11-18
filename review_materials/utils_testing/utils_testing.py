0fdb8f025d925df973cbc2d5917605cf717af30e
Mick Wright <mick.wright@protonmail.com>
2021-11-18 16:26:38 +0000

    Added additional comments to utils_testing.py

--- review_materials/utils_testing/utils_testing.py
+++ review_materials/utils_testing/utils_testing.py
@@ -33,30 +33,39 @@ y_file = gravelamps.lensing.utils.generate_source_position_file(config)
 print(f"Source Position file located at {y_file}")
 
 #Test the generation of the interpolator
+
+#Generate data covering dimensionless frequency and source position
 dim_freq_array = np.linspace(0, 2*np.pi, 1200)
 sour_pos_array = np.linspace(0, 2*np.pi, 1000)
 
+#Generating amplification factor data - using dummy sin functions 
 amp_fac_real_array = np.zeros((len(dim_freq_array), len(sour_pos_array)))
 amp_fac_imag_array = np.zeros(amp_fac_real_array.shape)
-
-print(amp_fac_real_array.shape)
 
 for i in range(len(sour_pos_array)):
     amp_fac_real_array[:,i] = np.sin(dim_freq_array)
 for j in range(len(dim_freq_array)):
     amp_fac_imag_array[j,:] = np.sin(sour_pos_array)
 
+#Create files containing test data
 np.savetxt("test_w.dat", dim_freq_array)
 np.savetxt("test_y.dat", sour_pos_array)
 np.savetxt("test_freal.dat", amp_fac_real_array)
 np.savetxt("test_fimag.dat", amp_fac_imag_array)
 
+#Generate interpolator
 interpolator = gravelamps.lensing.utils.generate_interpolator(
     "test_w.dat", "test_y.dat", "test_freal.dat", "test_fimag.dat")
 
+#Shift arrays to test interpolation 
 shifted_dim_freq = dim_freq_array + dim_freq_array[1]/2
 shifted_sour_pos = sour_pos_array + sour_pos_array[1]/2
 
+#Plots show the dimensionless frequency interpolation 
+#Left: Calculated values vs Interpolated 
+#Right: Residual between the two
+#Top: Using the exact data points used to generate interpolator
+#Bottom: Shifting the data to use non of the points used to generate interpolator
 plt.subplot(2,2,1)
 plt.plot(dim_freq_array, np.sin(dim_freq_array), label="Calculated")
 plt.plot(dim_freq_array, np.real(interpolator(dim_freq_array, 0)),
@@ -73,6 +82,7 @@ plt.subplot(2,2,4)
 plt.plot(shifted_dim_freq, np.real(interpolator(shifted_dim_freq, 0)) - np.sin(shifted_dim_freq))
 plt.show()
 
+#Same as above but for source position interpolation
 plt.subplot(2,2,1)
 plt.plot(sour_pos_array, np.sin(sour_pos_array), label="Caluclated")
 plt.plot(sour_pos_array, np.imag(interpolator(0, sour_pos_array)),
