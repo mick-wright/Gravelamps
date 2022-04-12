79bec42c08b82fabcee03dcca486e9c04cb46664
michael.wright <michael.wright@ligo.org>
2022-04-12 14:29:26 +0100

    Updated utils.py condensing the file generations into a single function

--- gravelamps/lensing/utils.py
+++ gravelamps/lensing/utils.py
@@ -10,67 +10,24 @@ Written by Mick Wright 2021
 import scipy.interpolate as scint
 import numpy as np
 
-def generate_dimensionless_frequency_file(config):
+def generate_value_file(config, value_type, output_filename):
     '''
     Input:
         config - INI configuration parser
+        value_type - Full name of the type of values to be genrated e.g dimensionless_frequency
+        output_filename - filepath to the file to generate
 
-    Output:
-        dim_freq_file - location of the generated dimensionless frequency file
-
-    Function from the options set in the INI, generates a file called "w.dat" in which there will
-    be an appropriately constructed list of dimensionless frequency values over which the
-    interpolator will be generated
+    File generates the seuqence of values for the type value under consideration saving it to the
+    specified filename
     '''
 
-    # Construct the full path to the file
-    outdir = config.get("output_settings", "outdir")
-    dim_freq_file = outdir + "/data/w.dat"
+    min_value = config.getfloat("lens_interpolation_settings", f"minimum_{value_type}")
+    max_value = config.getfloat("lens_interpolation_settings", f"maximum_{value_type}")
+    num_values = config.getfloat("lens_interpolation_settings", f"{value_type}_length")
 
-    # Get settings from the INI file determining the minimum and maximum values to consider
-    # as well as the number of points to generate
-    min_value = config.getfloat("lens_generation_settings", "minimum_dimensionless_frequency")
-    max_value = config.getfloat("lens_generation_settings", "maximum_dimensionless_frequency")
-    num_values = config.getint("lens_generation_settings", "dimensionless_frequency_length")
+    value_array = np.linspace(min_value, max_value, num_values)
 
-    # Generate the dimensionless frequency array
-    dim_freq_array = np.linspace(min_value, max_value, num_values)
-
-    # Save the resultant array to the filename
-    np.savetxt(dim_freq_file, dim_freq_array)
-
-    return dim_freq_file
-
-def generate_source_position_file(config):
-    '''
-    Input:
-        config - INI configuration parser
-
-    Output:
-        sour_pos_file - location of the generated source position file
-
-    Function from the options set in the INI, generates a file called "y.dat" in which there will
-    be an appropriately constructed list of source position values over which the interpolaotr
-    will be generated
-    '''
-
-    # Construct the full path to the file
-    outdir = config.get("output_settings", "outdir")
-    sour_pos_file = outdir + "/data/y.dat"
-
-    # Get settings from the INI file determining the minimum and maximum values to consider
-    # as well as the number of points to generate
-    min_value = config.getfloat("lens_generation_settings", "minimum_source_position")
-    max_value = config.getfloat("lens_generation_settings", "maximum_source_position")
-    num_values = config.getint("lens_generation_settings", "source_position_length")
-
-    # Generate the source position array
-    sour_pos_array = np.linspace(min_value, max_value, num_values)
-
-    # Save the resultant array to the filename
-    np.savetxt(sour_pos_file, sour_pos_array)
-
-    return sour_pos_file
+    np.savetxt(output_filename, value_array)
 
 def generate_interpolator(dim_freq_file, sour_pos_file, amp_fac_real_file, amp_fac_imag_file):
     '''
