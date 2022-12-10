25c98f96840be925faf3a7c3206cd3f9a3ea2127
Mick Wright <mick.wright@ligo.org>
2022-12-10 20:38:09 +0000

    Added dummy unlensed model

---
+++ gravelamps/lensing/unlensed.py
@@ -0,0 +1,12 @@
+'''
+Unlensed Functions
+
+Largely a dummy space to specify unlensed model.
+This will be empty aside from any functions that become necessary to perform unlensed runs
+
+Written by Mick Wright 2022
+'''
+
+_additional_arguments = []
+_additional_argument_types = []
+_lens_parameters = []
