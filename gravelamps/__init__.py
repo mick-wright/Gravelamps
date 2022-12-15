034ea2a39b05ae213fcea611d727e13d7018ebe3
Mick Wright <mick.wright@ligo.org>
2022-12-15 19:15:58 +0000

    Fixed circular import temporarily

--- gravelamps/__init__.py
+++ gravelamps/__init__.py
@@ -27,8 +27,9 @@ Contributors: Martin Hendry,
 
 from pkg_resources import get_distribution, DistributionNotFound
 
+import asimov
+
 from .core import conversion, file_handling, gravelog, graveparser, module_handling
-from . import asimov
 from . import lensing
 from . import inference
 
