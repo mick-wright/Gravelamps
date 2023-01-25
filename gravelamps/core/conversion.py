d15e2386c553b79e6fcc2da5b9109cb1d9f2f662
mick.wright <mick.wright@ligo.org>
2023-01-25 19:02:37 +0000

    conversion function docstrings rewritten

--- gravelamps/core/conversion.py
+++ gravelamps/core/conversion.py
@@ -1,28 +1,43 @@
-'''
-Gravelamps Conversion Functions
+"""Gravelamps Conversion Functions
 
-Functions within handle conversion of quantities from one unit to another
+Following are functions handling physical unit conversions
 
 Written by Mick Wright 2022
-'''
+
+Routines
+--------
+lens_mass_to_redshifted_lens_mass
+    Converts from source frame lens mass to observer frame redshfited lens msas
+solar_mass_to_natural_mass
+    Converts mass from units of solar mass to natural units such that c=G=1
+frequency_to_dimensionless_frequency
+    Converts regular frequencies in Hz to dimensionless frequencies
+
+"""
 
 from astropy.constants import c, G, M_sun
-
 import numpy as np
 
 from bilby.gw.conversion import luminosity_distance_to_redshift
 
 def lens_mass_to_redshifted_lens_mass(lens_mass, lens_fractional_distance, luminosity_distance):
-    '''
-    Input:
-        lens_mass - Source frame mass of lensing object in Solar Masses
-        lens_fractional_distance - Distance to lensing object as fraction of luminosty distance to
-                                   source
-        luminosity_distance - Distance to the source object in Mpc
+    """
+    Converts from source frame lens mass to observer frame redshifted lens mass
 
-    Output:
-        redshifted_lens_mass - Redshifted mass of the lensing object in natural units
-    '''
+    Parameters
+    ----------
+    lens_mass : float
+        Source frame mass of lensing object in Solar Masses
+    lens_fractional_distance : float
+        Distance to lensing object as a fraction of the luminosity distance to the source
+    luminosity_distance : float
+        Distance to the source object in Mpc
+
+    Returns
+    -------
+    redshifted_lens_mass : float
+        Observer frame mass of lensing object in natural units
+    """
 
     lens_distance = lens_fractional_distance * luminosity_distance
     lens_redshift = luminosity_distance_to_redshift(lens_distance)
@@ -31,15 +46,19 @@ def lens_mass_to_redshifted_lens_mass(lens_mass, lens_fractional_distance, lumin
     return redshifted_lens_mass
 
 def solar_mass_to_natural_mass(solar_mass):
-    '''
-    Input:
-        solar_mass - Mass in units of solar mass
+    """
+    Converts mass from units of solar mass to natural units such that c=G=1. 
 
-    Output:
-        natural_mnass - Mass in natural units
+    Parameters
+    ----------
+    solar_mass : float
+        Mass in solar mass units
 
-    Converts a mass from solar masses to natural units.
-    '''
+    Returns
+    -------
+    natural_mass : float
+        The same mass in natural units
+    """
 
     kg_mass = solar_mass * M_sun
     natural_mass = kg_mass * (G/c**3)
@@ -48,19 +67,24 @@ def solar_mass_to_natural_mass(solar_mass):
 
 def frequency_to_dimensionless_frequency(frequency_array,
                                          redshifted_lens_mass=None):
-    '''
-    Input:
-        frequency_array - Array of frequencies for gravitational wave data
-        redshifted_lens_mass - Observer frame mass of the lensing object in natural units
+    """
+    Converts regular frequencies in Hz to dimensionless frequencies.
 
-    Output;
-        dimensionless_frequency_array - Corresponding array of dimensionless frequencies
-                                        associated with the frequency array and the lensing object
+    Can be done in two methods. If the redshifted lens mass is supplied this will be done for that
+    specific mass object. If it is not supplied, it will be done using a generic agnostic form
 
-    Function converts an arary of frequencies to the corresponding dimensionless frequencies
-    with either the specified object mass or to a generic agnostic form if object mass is not
-    specified
-    '''
+    Parameters
+    ----------
+    frequency_array : ArrayLike
+        Array of frequencies to convert
+    redshifted_lens_mass : float, optional
+        Mass of lensing object to use in conversion
+
+    Returns
+    -------
+    dimensionless_frequency_array : ArrayLike
+        Array of converted dimensionless frequencies
+    """
 
     if redshifted_lens_mass is not None:
         dimensionless_frequency_array = 8 * np.pi * redshifted_lens_mass * frequency_array
